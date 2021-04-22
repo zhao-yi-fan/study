442f6ef8b184e99bcf808f128f631eede7488ff9
zhaoyifan <993445491@qq.com>
2021-04-22 22:55:59 +0800

    提交

--- cross-domain-public/2.cors/server2.js
+++ cross-domain-public/2.cors/server2.js
@@ -6,10 +6,13 @@ app.use(function (req, res, next) {
   if (whiteList.includes(origin)) {
     // 设置哪个源可以访问
     res.setHeader('Access-Control-Allow-Origin', origin)
-    res.setHeader('Access-Control-Allow-Headers', 'name')
-    res.setHeader('Access-Control-Allow-Methods', 'PUT'); // PUT会做一个OPSITONS预检请求
-    res.setHeader('Access-Control-Max-Age', 6); // 预检测不是每次都发，而是过6秒发一次，默认是一分钟发一次
-    if (req.method === 'PUST') {
+    res.setHeader('Access-Control-Allow-Headers', 'name'); // 多个 'name,a,b,c'     对应 xhr.setRequestHeader('name', 'zfpx');
+    // 跨域默认只有GET/POST/HEAD 其他请求方法需要标明
+    res.setHeader('Access-Control-Allow-Methods', 'PUT');
+    // 跨域的话 除了简单请求方法(GET/POST/HEAD)都会做一个OPSITONS预检请求
+    // 预检测不是每次都发，而是过6秒发一次，默认是一分钟发一次
+    res.setHeader('Access-Control-Max-Age', 6); // 以秒为单位
+    if (req.method === 'PUT') {
       res.end(); // OPTIONS请求不做任何处理
     }
   }
