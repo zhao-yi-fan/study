a9950d793c7bcef7b49170b92e704ea795aee168
zhaoyifan <zhaoyifan@fehorizon.com>
2021-08-19 16:17:53 +0800

    router-view

--- vue-router_raw/src/views/About.vue
+++ vue-router_raw/src/views/About.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="about">
+    <h1>这是about页</h1>
+
     <router-link to="/about/a"></router-link>
     <router-link to="/about/b"></router-link>
+    
     <router-view />
   </div>
 </template>
