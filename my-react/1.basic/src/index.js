acb6e379fb7621ee01cffa21238e500240805755
zhaoyifan <993445491@qq.com>
2022-01-17 23:59:12 +0800

    feat: ReactDOM.render()

--- my-react/1.basic/src/index.js
+++ my-react/1.basic/src/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-// import ReactDOM from 'react-dom'
+import React from './react'
+import ReactDOM from './react-dom'
 
 // React.createElement会返回一个React元素 
 // react元素是一个普通的js对象，它描述了dom的内容，常说的虚拟dom
@@ -8,17 +8,17 @@ import React from 'react'
 // babel并没有把JSX编译成虚拟DOM，而是把JSX编译成了React.createElement的方法调用。
 /*#__PURE__*/  // 该注释是用来实现tree-shaking
 let element1 = React.createElement("h1", {
-	className: "title",
-	style: {
-		color: 'red'
-	}
-}, "hello");
+  className: "title",
+  style: {
+    color: 'red'
+  }
+}, "hello", React.createElement('span', null, 'world'));
 
 // ReactDOM.render会负责渲染，把React元素渲染到DOM容器内（root）
-/* ReactDOM.render(
-	// <h1 className='title' style={{ color: 'red' }}>hello</h1>
-	element1, document.getElementById('root')
-) */
+ReactDOM.render(
+  // <h1 className='title' style={{ color: 'red' }}>hello</h1>
+  element1, document.getElementById('root')
+)
 
 console.log(JSON.stringify(element1, null, 2));
 
---
+++ my-react/1.basic/src/react-dom.js
@@ -0,0 +1,90 @@
+import { REACT_TEXT } from "./constants";
+
+/**
+ * 把虚拟DOM变成真实DOM插入到容器内部
+ * @param {*} vdom 虚拟DOM
+ * @param {*} container 容器
+ */
+function render (vdom, container) {
+  mount(vdom, container)
+}
+
+function mount (vdom, parentDOM) {
+  let newDOM = createDOM(vdom)
+  if (newDOM) {
+    parentDOM.appendChild(newDOM)
+
+  }
+  return newDOM;
+}
+
+/**
+ * 把虚拟DOM转成真实DOM
+ */
+function createDOM (vdom) {
+  if (!vdom) return null;
+  let { type, props } = vdom;
+  let dom; // 真实DOM
+  if (type === REACT_TEXT) { // 如果这个元素是一个文本的话
+    dom = document.createTextNode(props.content)
+  } else {
+    dom = document.createElement(type); // div span p
+  }
+  // 处理属性
+  if (props) {
+    updateProps(dom, {}, props); // 更新属性
+    if (props.children) {
+      let children = props.children;
+      if (typeof children === 'object' && children.type) { // 说明这是一个React元素
+        mount(children, dom)
+      } else if (Array.isArray(children)) {
+        reconcileChildren(props.children, dom)
+
+      }
+    }
+  }
+  return dom;
+}
+
+function reconcileChildren (childrenVdom, parentDOM) {
+  childrenVdom.forEach(childVdom => mount(childVdom, parentDOM));
+}
+
+/**
+ * 把新的属性更新到真实DOM上
+ * @param {*} dom 真实DOM
+ * @param {*} oldProps 旧的属性对象
+ * @param {*} newProps 新的属性对象
+ */
+function updateProps (dom, oldProps, newProps) {
+  for (let key in newProps) {
+    if (key === 'children') {
+      continue; // 此处忽略子节点的处理
+    } else if (key === 'style') { // style
+      let styleObj = newProps[key];
+      for (let attr in styleObj) {
+        dom.style[attr] = styleObj[attr];
+      }
+    } else {
+      dom[key] = newProps[key]; // className
+    }
+
+  }
+}
+
+
+const ReactDOM = {
+  render
+
+}
+
+export default ReactDOM
+
+
+
+
+
+
+
+
+
