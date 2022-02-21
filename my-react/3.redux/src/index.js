c28723ea7f62dbbbe6267cf1dc9c6cff1faefed6
zhaoyifan <993445491@qq.com>
2022-02-21 22:29:17 +0800

    redux

---
+++ my-react/3.redux/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter1 from './components/Counter1'
+import Counter2 from './components/Counter2'
+
+ReactDOM.render(
+  <>
+    <Counter1 />
+    <Counter2 />
+  </>
+  ,
+  document.getElementById('root')
+);
+
