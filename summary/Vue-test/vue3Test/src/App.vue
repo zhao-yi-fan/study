ef4bbec5223acf2f60b5212b5e5a07b6819862a5
zhaoyifan <993445491@qq.com>
2022-06-12 23:20:11 +0800

    reactive解包

--- summary/Vue-test/vue3Test/src/App.vue
+++ summary/Vue-test/vue3Test/src/App.vue
@@ -1,49 +1,29 @@
 <script setup>
 import { isRef, reactive, ref, toRefs, unref } from "vue";
-const numRef = ref(0);
+let count = ref(0);
 const customObj = {
-  name: "张三",
-  age: 18,
-  hobby: ["游泳", "爬山"],
+  count,
 };
-const state = reactive(customObj);
-const stateAsRefs = toRefs(state);
+const state = reactive({
+  count,
+});
 
-console.log(state,'state===');
-console.log({...state});
-const fn = () => {
-  numRef.value++;
-  console.log(numRef);
-};
-const unrefFn = () => {
-  console.log(isRef(numRef));
-  console.log(unref(numRef));
-};
-const stateFn = () => {
-  state.name = "李四";
-
-  console.log(customObj, "customObj===");
-  console.log(stateAsRefs, "stateAsRefs===");
-};
-const stateChange = () => {
-  numRef++;
-};
+console.log(count, "count===");
+console.log(state, "state====");
+console.log(state.count, "state.count====");
+console.log(state.count === count.value); // true
+const stateFn = () => {};
 </script>
 
 <template>
-  <button @click="stateChange">stateChange</button>
-  <br />
-  <button @click="fn">+1</button>
-  <br />
-  <button @click="unrefFn">unref</button>
-  <br />
   <button @click="stateFn">state</button>
   <br />
-  <span>{{ numRef }}</span>
+  <span>{{ count }}</span>
+  <br />
+  <span>customObj:{{ customObj }}</span>
   <br />
   <span>state:{{ state }}</span>
   <br />
-  <span>customObj:{{ customObj }}</span>
 </template>
 
 <style></style>
