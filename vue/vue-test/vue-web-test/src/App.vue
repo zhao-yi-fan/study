3ea786656ba1662ed210a66d075ce2e364898b47
zhaoyifan <zhaoyifan@fehorizon.com>
2022-11-18 18:20:18 +0800

    feat: vue webpack配置

--- vue/vue-test/vue-web-test/src/App.vue
+++ vue/vue-test/vue-web-test/src/App.vue
@@ -3,28 +3,35 @@
     {{ obj.a }}
     {{ obj.b }}
     <button @click="onclick">点我+1</button>
+    <div v-for="item in list">
+      <p>{{ item.text }}</p>
+    </div>
   </div>
 </template>
 <script>
+console.log(module);
 export default {
   name: "App",
   data() {
     return {
       obj: {
-        a: 1,
+        a: 3,
         b: 2,
       },
+      list: [{ text: 1 }],
     };
   },
   methods: {
     onclick() {
-      this.obj.a++;
+      this.list.push({ text: 2 });
+      console.log(this.list);
     },
   },
 };
 </script>
 <style lang="scss">
+$color: red;
 .aa {
-  background-color: red;
+  background-color: $color;
 }
 </style>
--- vue/vue-test/vue-web-test/webpack.config.js
+++ vue/vue-test/vue-web-test/webpack.config.js
@@ -1,21 +1,26 @@
 const path = require('path')
+const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+console.log(process.env.NODE_ENV);
+
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/main.js',
   output: {
-    filename: 'build.js',
+    filename: 'build.[hash:8].js',
     path: path.resolve(__dirname, 'build'),
   },
-  watch: true,
-  watchOptions: {
-    aggregateTimeout: 20000,
-    poll: 1000
-  },
+  // watch: true,
+  // watchOptions: {
+  //   aggregateTimeout: 20000,
+  //   poll: 1000
+  // },
   devServer: {
     port: 8088,
-    progress: true, // 加进度条
+    // progress: true, // 加进度条
+    hot: true,
     contentBase: './build', // 在哪个文件夹下起服务
     // open: true,
     compress: true, // gzip压缩
@@ -26,17 +31,25 @@ module.exports = {
     },
     extensions: ['.js', '.vue']
   },
+  resolveLoader: {
+    modules: [path.resolve(__dirname, './loaders'), 'node_modules']
+  },
   plugins: [
     new VueLoaderPlugin(),
     new HtmlWebPackPlugin({
       template: './public/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin(), // 热更新插件
+    new webpack.NamedChunksPlugin(), // 打印更新的模块路径
+    new MiniCssExtractPlugin({
+      filename: 'main.css'
     })
   ],
   module: {
     rules: [
       {
         test: /\.vue$/,
-        use: ['sass-loader', 'vue-loader']
+        use: ['vue-loader']
       },
       {
         test: /\.js$/,
@@ -50,10 +63,26 @@ module.exports = {
         ]
       },
       {
-        test: /\.c[ac]ss$/i,
+        test: /\.scss$/i,
         use: [
-          'style-loader',
+          MiniCssExtractPlugin.loader,
+          // 'style-loader',
           'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "autoprefixer",
+                    {
+                      // Options
+                    },
+                  ],
+                ],
+              },
+            },
+          },
           'sass-loader'
         ]
       }
