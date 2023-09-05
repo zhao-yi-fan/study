a06ed590fafbf762e0f2f68799577286dc699b3c
zhaoyifan <993445491@qq.com>
2023-09-06 00:13:49 +0800

    feat: zmp

---
+++ zmp/examples/host/src/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import RemoteApp from '@remote/App';
+const App = () => {
+  return React.createElement(RemoteApp);
+}；
+export default App;
---
+++ zmp/examples/remote/src/App.js
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <h1>Remote App</h1>
+  );
+};
+
+export default App
