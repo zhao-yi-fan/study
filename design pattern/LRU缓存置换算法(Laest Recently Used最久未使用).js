ea27939492b7dee09b2947d287648b66478547ea
zhaoyifan <993445491@qq.com>
2024-03-03 20:20:37 +0800

    chore: LRU缓存置换算法

---
+++ design pattern/LRU缓存置换算法(Laest Recently Used最久未使用).js
@@ -0,0 +1,35 @@
+class LRUCache {
+  #length = 0;
+  #map;
+  constructor(len) {
+    this.#map = new Map();
+    this.#length = len;
+  }
+  has(key) {
+    return this.#map.has(key);
+  }
+  get(key) {
+    if (!this.has(key)) return null;
+    const value = this.#map.get(key);
+    this.#map.delete(key);
+    this.#map.set(key, value);
+    return value;
+  }
+  set(key, value) {
+    if (this.has(key)) {
+      this.#map.delete(key);
+    }
+    this.#map.set(key, value);
+    if (this.#map.size > this.#length) {
+      this.#map.delete(this.#map.keys().next().value);
+    }
+    return this.#map;
+  }
+}
+
+const lru = new LRUCache(2);
+console.log(lru.set("aa", 111));
+console.log(lru.set("aa", 222));
+console.log(lru.set("bb", 222));
+console.log(lru.get("aa"));
+console.log(lru.set("cc", 333));
