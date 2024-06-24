64cf78ce4ffe6b99704c6402041f753f1d03ffd3
zhaoyifan <zhaoyifan@fehorizon.com>
2024-06-24 18:44:10 +0800

    chore: 冒泡排序

--- design pattern/bubble sort.js
+++ design pattern/bubble sort.js
@@ -1,11 +1,13 @@
-var arr = [20, 10, 66, 44];
-for (i = 0; i < arr.length - 1; i++) {
-  for (j = 0; j < arr.length - 1 - i; j++) {
-    if (arr[j] > arr[j + 1]) {
-      var temp = arr[j + 1];
-      arr[j + 1] = arr[j];
-      arr[j] = temp;
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
     }
   }
+  return arr;
 }
-console.log(arr);
+console.log(bubbleSort([20, 10, 66, 44]));
